f3004542cfdcf1ee9734290e6b275fcd101bb5cb
byuengyun Chae <chaeby7@gmail.com>
2022-04-26 22:58:53 +0900

    Open Port

--- src/server.js
+++ src/server.js
@@ -1,3 +1,8 @@
 import express from "express";
 
+const app = express();
+const PORT = 4000;
+const handleListening = () =>
+  console.log(`Server listenting on port http://localhost:${PORT} 🚀`);
 
+app.listen(PORT, handleListening);
